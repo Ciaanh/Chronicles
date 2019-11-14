475502ed169ec45f993d15b37b7ddf90c537b263
Nicolas Fabre <nicolas.fabre@younited-credit.fr>
2019-11-14 10:39:15 +0100

    Refactor Timeline, add event list selected values

--- Core.lua
+++ Core.lua
@@ -12,31 +12,25 @@ Chronicles.descName = L["Chronicles"]
 Chronicles.description = L["Display Azeroth history as a timeline"]
 
 Chronicles.SelectedValues = {
-	currentTimelinePage = 1,
-	currentTimelineYear = Chronicles.constants.timeline.yearStart,
-	timelineStep = Chronicles.constants.timeline.defaultStep,
-	eventId = nil
+    currentEventListPage = 1,
+    eventListData = nil,
+    selectedEvent = nil,
+
+    currentTimelinePage = 1,
+    selectedTimelineYear = Chronicles.constants.timeline.yearStart,
+    timelineStep = Chronicles.constants.timeline.defaultStep
 }
 
 local _db
 local _options
 local _defaults = {
-	global = {
-		options = {
-			version = "",
-			minimap = {
-				hide = false
-			}
-		}
-	}
+    global = {options = {version = "", minimap = {hide = false}}}
 }
 
 function tablelength(T)
-	local count = 0
-	for _ in pairs(T) do
-		count = count + 1
-	end
-	return count
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
 end
 
 ----------------------------------------------------------------------------------------
@@ -44,115 +38,103 @@ end
 Chronicles.UI = {}
 Chronicles.pluginsDB = {}
 
-function Chronicles.UI:Init()
-	Chronicles.UI.Timeline:DsiplayTimeline()
-end
+function Chronicles.UI:Init() Chronicles.UI.Timeline:DsiplayTimeline() end
 
-function Chronicles.UI:DisplayWindow()
-	Chronicles_UI:Show()
-end
+function Chronicles.UI:DisplayWindow() Chronicles_UI:Show() end
 
-function Chronicles.UI:HideWindow()
-	Chronicles_UI:Hide()
-end
+function Chronicles.UI:HideWindow() Chronicles_UI:Hide() end
 
 function Chronicles:OnInitialize()
-	self.db = LibStub("AceDB-3.0"):New("ChroniclesDB", _defaults, true)
+    self.db = LibStub("AceDB-3.0"):New("ChroniclesDB", _defaults, true)
 
-	self.mapIcon =
-		LibStub("LibDataBroker-1.1"):NewDataObject(
-		FOLDER_NAME,
-		{
-			type = "launcher",
-			text = "Chronicles",
-			icon = "Interface\\ICONS\\Inv_scroll_04",
-			OnClick = function(self, button, down)
-				if (Chronicles_UI:IsVisible()) then
-					Chronicles.UI:HideWindow()
-				else
-					Chronicles.UI:DisplayWindow()
-				end
-			end,
-			OnTooltipShow = function(tt)
-				tt:AddLine("Chronicles", 1, 1, 1)
-				tt:AddLine("Click to show the timeline.")
-			end
-		}
-	)
-	I:Register(FOLDER_NAME, self.mapIcon, self.db.global.options.minimap)
+    self.mapIcon = LibStub("LibDataBroker-1.1"):NewDataObject(FOLDER_NAME, {
+        type = "launcher",
+        text = "Chronicles",
+        icon = "Interface\\ICONS\\Inv_scroll_04",
+        OnClick = function(self, button, down)
+            if (Chronicles_UI:IsVisible()) then
+                Chronicles.UI:HideWindow()
+            else
+                Chronicles.UI:DisplayWindow()
+            end
+        end,
+        OnTooltipShow = function(tt)
+            tt:AddLine("Chronicles", 1, 1, 1)
+            tt:AddLine("Click to show the timeline.")
+        end
+    })
+    I:Register(FOLDER_NAME, self.mapIcon, self.db.global.options.minimap)
 
-	Chronicles.UI:Init()
+    Chronicles.UI:Init()
 
-	self:RegisterChatCommand(
-		"chronicles",
-		function()
-			self.UI:DisplayWindow()
-		end
-	)
+    self:RegisterChatCommand("chronicles",
+                             function() self.UI:DisplayWindow() end)
 end
 
 -- External DB tools -------------------------------------------------------------------
 function Chronicles:RegisterPluginDB(pluginName, db)
-	--DEFAULT_CHAT_FRAME:AddMessage("-- Asked to register plugin " .. pluginName)
-	if self.pluginsDB[pluginName] ~= nil then
-		error(pluginName .. " is already registered by another plugin.")
-	else
-		self.pluginsDB[pluginName] = db
-	end
+    -- DEFAULT_CHAT_FRAME:AddMessage("-- Asked to register plugin " .. pluginName)
+    if self.pluginsDB[pluginName] ~= nil then
+        error(pluginName .. " is already registered by another plugin.")
+    else
+        self.pluginsDB[pluginName] = db
+    end
 end
 -----------------------------------------------------------------------------------------
 
 -- Should return a list of objects :
 -- { label, yearStart, yearEnd, description, eventType, icon, source }
 function Chronicles:SearchEvents(yearStart, yearEnd)
-	local foundEvents = {}
-	if (yearStart <= yearEnd) then
-		for pluginName in pairs(self.pluginsDB) do
-			local pluginEvents = Chronicles:SearchEventsInDB(yearStart, yearEnd, self.pluginsDB[pluginName])
+    local foundEvents = {}
+    if (yearStart <= yearEnd) then
+        for pluginName in pairs(self.pluginsDB) do
+            local pluginEvents = Chronicles:SearchEventsInDB(yearStart, yearEnd,
+                                                             self.pluginsDB[pluginName])
 
-			for eventIndex in pairs(pluginEvents) do
-				local event = pluginEvents[eventIndex]
-				table.insert(foundEvents, Chronicles:CleanEventObject(event, pluginName))
-			end
-		end
-	end
-	DEFAULT_CHAT_FRAME:AddMessage("-- Found events " .. tablelength(foundEvents))
-	return foundEvents
+            for eventIndex in pairs(pluginEvents) do
+                local event = pluginEvents[eventIndex]
+                table.insert(foundEvents,
+                             Chronicles:CleanEventObject(event, pluginName))
+            end
+        end
+    end
+    DEFAULT_CHAT_FRAME:AddMessage("-- Found events " .. tablelength(foundEvents))
+    return foundEvents
 end
 
 function Chronicles:SearchEventsInDB(yearStart, yearEnd, db)
-	local selectedEvents = {}
-	for eventIndex in pairs(db) do
-		if Chronicles:IsInRange(db[eventIndex], yearStart, yearEnd) then
-			table.insert(selectedEvents, db[eventIndex])
-		end
-	end
-	return selectedEvents
+    local selectedEvents = {}
+    for eventIndex in pairs(db) do
+        if Chronicles:IsInRange(db[eventIndex], yearStart, yearEnd) then
+            table.insert(selectedEvents, db[eventIndex])
+        end
+    end
+    return selectedEvents
 end
 
 function Chronicles:IsInRange(event, yearStart, yearEnd)
-	if (yearStart <= event.yearStart and event.yearStart <= yearEnd) then
-		return true
-	end
-	if (yearStart <= event.yearEnd and event.yearEnd <= yearEnd) then
-		return true
-	end
-	return false
+    if (yearStart <= event.yearStart and event.yearStart <= yearEnd) then
+        return true
+    end
+    if (yearStart <= event.yearEnd and event.yearEnd <= yearEnd) then
+        return true
+    end
+    return false
 end
 
 function Chronicles:CleanEventObject(event, pluginName)
-	if event then
-		local description = event.description or UNKNOWN
-		local icon = event.icon or private.constants.defaultIcon
+    if event then
+        local description = event.description or UNKNOWN
+        local icon = event.icon or private.constants.defaultIcon
 
-		return {
-			label = event.label,
-			yearStart = event.yearStart,
-			yearEnd = event.yearEnd,
-			description = description,
-			eventType = event.eventType,
-			icon = icon,
-			source = pluginName
-		}
-	end
+        return {
+            label = event.label,
+            yearStart = event.yearStart,
+            yearEnd = event.yearEnd,
+            description = description,
+            eventType = event.eventType,
+            icon = icon,
+            source = pluginName
+        }
+    end
 end
--- UI/Timeline.lua
+++ UI/Timeline.lua
@@ -3,16 +3,7 @@ local Chronicles = private.Core
 
 Chronicles.UI.Timeline = {}
 
-Chronicles.UI.Timeline.StepValues = {
-	1000,
-	500,
-	250,
-	100,
-	50,
-	10,
-	5,
-	1
-}
+Chronicles.UI.Timeline.StepValues = {1000, 500, 250, 100, 50, 10, 5, 1}
 
 Chronicles.UI.Timeline.MaxStepIndex = 3
 
@@ -21,118 +12,140 @@ Chronicles.UI.Timeline.MaxStepIndex = 3
 ------------------------------------------------------------------------------------------
 
 function Chronicles.UI.Timeline:DsiplayTimeline()
-	Chronicles.UI.Timeline:DisplayTimelinePage(Chronicles.SelectedValues.currentTimelinePage)
-end
-
-function GetLowerBound(index)
-	local value = Chronicles.constants.timeline.yearStart + ((index - 1) * Chronicles.SelectedValues.timelineStep)
-
-	if (value < Chronicles.constants.timeline.yearStart) then
-		return Chronicles.constants.timeline.yearStart
-	end
-	return value
-end
-
-function GetUpperBound(index)
-	local value = Chronicles.constants.timeline.yearStart + (index * Chronicles.SelectedValues.timelineStep) - 1
-	if (value > Chronicles.constants.timeline.yearEnd) then
-		return Chronicles.constants.timeline.yearEnd
-	end
-	return value
+    Chronicles.UI.Timeline:DisplayTimelinePage(
+        Chronicles.SelectedValues.currentTimelinePage)
 end
 
 -- page goes from 1 to math.floor(numberOfCells / pageSize)
 -- index should go from 1 to GetNumberOfTimelineBlock
 function Chronicles.UI.Timeline:DisplayTimelinePage(page)
-	local pageSize = Chronicles.constants.timeline.pageSize
-	local numberOfCells = GetNumberOfTimelineBlock(Chronicles.SelectedValues.timelineStep)
-	local maxPageValue = math.ceil(numberOfCells / pageSize)
+    local pageSize = Chronicles.constants.timeline.pageSize
+    local numberOfCells = GetNumberOfTimelineBlock(
+                              Chronicles.SelectedValues.timelineStep)
+    local maxPageValue = math.ceil(numberOfCells / pageSize)
 
-	if (page < 1) then
-		page = 1
-	end
-	if (page > maxPageValue) then
-		page = maxPageValue
-	end
+    if (page < 1) then page = 1 end
+    if (page > maxPageValue) then page = maxPageValue end
 
-	--DEFAULT_CHAT_FRAME:AddMessage("-- Asked page " .. page)
-	--DEFAULT_CHAT_FRAME:AddMessage("-- SetMinMaxValues " .. numberOfCells .. "  " .. pageSize .. "  " .. maxPageValue)
+    -- DEFAULT_CHAT_FRAME:AddMessage("-- Asked page " .. page)
+    -- DEFAULT_CHAT_FRAME:AddMessage("-- SetMinMaxValues " .. numberOfCells .. "  " .. pageSize .. "  " .. maxPageValue)
 
-	TimelineScrollBar:SetMinMaxValues(1, maxPageValue)
-	Chronicles.SelectedValues.currentTimelinePage = page
+    TimelineScrollBar:SetMinMaxValues(1, maxPageValue)
+    Chronicles.SelectedValues.currentTimelinePage = page
 
-	if (numberOfCells <= pageSize) then
-		TimelinePreviousButton:Disable()
-		TimelineNextButton:Disable()
-	else
-		TimelinePreviousButton:Enable()
-		TimelineNextButton:Enable()
-	end
+    if (numberOfCells <= pageSize) then
+        TimelinePreviousButton:Disable()
+        TimelineNextButton:Disable()
+    else
+        TimelinePreviousButton:Enable()
+        TimelineNextButton:Enable()
+    end
 
-	local firstIndex = 1 + ((page - 1) * pageSize)
-	--DEFAULT_CHAT_FRAME:AddMessage("-- FirstIndex " .. firstIndex)
+    local firstIndex = 1 + ((page - 1) * pageSize)
+    -- DEFAULT_CHAT_FRAME:AddMessage("-- FirstIndex " .. firstIndex)
 
-	if (firstIndex <= 1) then
-		firstIndex = 1
-		TimelinePreviousButton:Disable()
-		Chronicles.SelectedValues.currentTimelinePage = 1
-	end
+    if (firstIndex <= 1) then
+        firstIndex = 1
+        TimelinePreviousButton:Disable()
+        Chronicles.SelectedValues.currentTimelinePage = 1
+    end
 
-	if ((firstIndex + 7) >= numberOfCells) then
-		firstIndex = numberOfCells - 7
-		TimelineNextButton:Disable()
-		Chronicles.SelectedValues.currentTimelinePage = maxPageValue
-	end
+    if ((firstIndex + 7) >= numberOfCells) then
+        firstIndex = numberOfCells - 7
+        TimelineNextButton:Disable()
+        Chronicles.SelectedValues.currentTimelinePage = maxPageValue
+    end
 
-	TimelineScrollBar:SetValue(Chronicles.SelectedValues.currentTimelinePage)
+    TimelineScrollBar:SetValue(Chronicles.SelectedValues.currentTimelinePage)
 
-	--DEFAULT_CHAT_FRAME:AddMessage("-- FirstIndexAfterChecked " .. firstIndex)
-	--DEFAULT_CHAT_FRAME:AddMessage("-- Page and Index " .. Chronicles.SelectedValues.currentTimelinePage .. "  " .. firstIndex)
+    -- DEFAULT_CHAT_FRAME:AddMessage("-- FirstIndexAfterChecked " .. firstIndex)
+    -- DEFAULT_CHAT_FRAME:AddMessage("-- Page and Index " .. Chronicles.SelectedValues.currentTimelinePage .. "  " .. firstIndex)
 
-	-- TimelineBlock1
-	local lowerBoundBlock1 = GetLowerBound(firstIndex)
-	local upperBoundBlock1 = GetUpperBound(firstIndex)
-	SetTextToFrame(lowerBoundBlock1, upperBoundBlock1, TimelineBlock1)
+    -- TimelineBlock1
+    local lowerBoundBlock1 = GetLowerBound(firstIndex)
+    local upperBoundBlock1 = GetUpperBound(firstIndex)
+    SetTextToFrame(lowerBoundBlock1, upperBoundBlock1, TimelineBlock1)
 
-	-- TimelineBlock2
-	local lowerBoundBlock2 = GetLowerBound(firstIndex + 1)
-	local upperBoundBlock2 = GetUpperBound(firstIndex + 1)
-	SetTextToFrame(lowerBoundBlock2, upperBoundBlock2, TimelineBlock2)
+    -- TimelineBlock2
+    local lowerBoundBlock2 = GetLowerBound(firstIndex + 1)
+    local upperBoundBlock2 = GetUpperBound(firstIndex + 1)
+    SetTextToFrame(lowerBoundBlock2, upperBoundBlock2, TimelineBlock2)
 
-	-- TimelineBlock3
-	local lowerBoundBlock3 = GetLowerBound(firstIndex + 2)
-	local upperBoundBlock3 = GetUpperBound(firstIndex + 2)
-	SetTextToFrame(lowerBoundBlock3, upperBoundBlock3, TimelineBlock3)
+    -- TimelineBlock3
+    local lowerBoundBlock3 = GetLowerBound(firstIndex + 2)
+    local upperBoundBlock3 = GetUpperBound(firstIndex + 2)
+    SetTextToFrame(lowerBoundBlock3, upperBoundBlock3, TimelineBlock3)
 
-	-- TimelineBlock4
-	local lowerBoundBlock4 = GetLowerBound(firstIndex + 3)
-	local upperBoundBlock4 = GetUpperBound(firstIndex + 3)
-	SetTextToFrame(lowerBoundBlock4, upperBoundBlock4, TimelineBlock4)
+    -- TimelineBlock4
+    local lowerBoundBlock4 = GetLowerBound(firstIndex + 3)
+    local upperBoundBlock4 = GetUpperBound(firstIndex + 3)
+    SetTextToFrame(lowerBoundBlock4, upperBoundBlock4, TimelineBlock4)
 
-	-- TimelineBlock5
-	local lowerBoundBlock5 = GetLowerBound(firstIndex + 4)
-	local upperBoundBlock5 = GetUpperBound(firstIndex + 4)
-	SetTextToFrame(lowerBoundBlock5, upperBoundBlock5, TimelineBlock5)
+    -- TimelineBlock5
+    local lowerBoundBlock5 = GetLowerBound(firstIndex + 4)
+    local upperBoundBlock5 = GetUpperBound(firstIndex + 4)
+    SetTextToFrame(lowerBoundBlock5, upperBoundBlock5, TimelineBlock5)
 
-	-- TimelineBlock6
-	local lowerBoundBlock6 = GetLowerBound(firstIndex + 5)
-	local upperBoundBlock6 = GetUpperBound(firstIndex + 5)
-	SetTextToFrame(lowerBoundBlock6, upperBoundBlock6, TimelineBlock6)
+    -- TimelineBlock6
+    local lowerBoundBlock6 = GetLowerBound(firstIndex + 5)
+    local upperBoundBlock6 = GetUpperBound(firstIndex + 5)
+    SetTextToFrame(lowerBoundBlock6, upperBoundBlock6, TimelineBlock6)
 
-	-- TimelineBlock7
-	local lowerBoundBlock7 = GetLowerBound(firstIndex + 6)
-	local upperBoundBlock7 = GetUpperBound(firstIndex + 6)
-	SetTextToFrame(lowerBoundBlock7, upperBoundBlock7, TimelineBlock7)
+    -- TimelineBlock7
+    local lowerBoundBlock7 = GetLowerBound(firstIndex + 6)
+    local upperBoundBlock7 = GetUpperBound(firstIndex + 6)
+    SetTextToFrame(lowerBoundBlock7, upperBoundBlock7, TimelineBlock7)
 
-	-- TimelineBlock8
-	local lowerBoundBlock8 = GetLowerBound(firstIndex + 7)
-	local upperBoundBlock8 = GetUpperBound(firstIndex + 7)
-	SetTextToFrame(lowerBoundBlock8, upperBoundBlock8, TimelineBlock8)
+    -- TimelineBlock8
+    local lowerBoundBlock8 = GetLowerBound(firstIndex + 7)
+    local upperBoundBlock8 = GetUpperBound(firstIndex + 7)
+    SetTextToFrame(lowerBoundBlock8, upperBoundBlock8, TimelineBlock8)
 end
 
 function GetNumberOfTimelineBlock()
-	local length = math.abs(Chronicles.constants.timeline.yearStart - Chronicles.constants.timeline.yearEnd)
-	return math.ceil(length / Chronicles.SelectedValues.timelineStep)
+    local length = math.abs(Chronicles.constants.timeline.yearStart -
+                                Chronicles.constants.timeline.yearEnd)
+    return math.ceil(length / Chronicles.SelectedValues.timelineStep)
+end
+
+function GetLowerBound(blockIndex)
+    local value = Chronicles.constants.timeline.yearStart +
+                      ((blockIndex - 1) * Chronicles.SelectedValues.timelineStep)
+
+    if (value < Chronicles.constants.timeline.yearStart) then
+        return Chronicles.constants.timeline.yearStart
+    end
+    return value
+end
+
+function GetUpperBound(blockIndex)
+    local value = Chronicles.constants.timeline.yearStart +
+                      (blockIndex * Chronicles.SelectedValues.timelineStep) - 1
+    if (value > Chronicles.constants.timeline.yearEnd) then
+        return Chronicles.constants.timeline.yearEnd
+    end
+    return value
+end
+
+function SetTextToFrame(blockIndex, frame)
+    local lowerBoundBlock = GetLowerBound(blockIndex)
+    local upperBoundBlock = GetUpperBound(blockIndex)
+
+    local text = "" .. lowerBoundBlock .. "\n" .. upperBoundBlock
+    frame.lowerBound = lowerBoundBlock
+    frame.upperBound = upperBoundBlock
+
+    local label = _G[frame:GetName() .. "Text"]
+    label:SetText(text)
+
+    frame:SetScript("OnMouseDown", function()
+        local eventList = Chronicles:SearchEvents(lowerBoundBlock,
+                                                  upperBoundBlock)
+        Chronicles.SelectedValues.selectedTimelineYear =
+            math.floor((lowerBound + upperBound) / 2)
+        Chronicles.UI.EventList:DrawEventList(frame.lowerBound,
+                                              frame.upperBound, eventList)
+    end)
 end
 
 ------------------------------------------------------------------------------------------
@@ -140,52 +153,51 @@ end
 ------------------------------------------------------------------------------------------
 
 function GetStepValueIndex(stepValue)
-	local index = {}
-	for k, v in pairs(Chronicles.UI.Timeline.StepValues) do
-		index[v] = k
-	end
-	return index[stepValue]
+    local index = {}
+    for k, v in pairs(Chronicles.UI.Timeline.StepValues) do index[v] = k end
+    return index[stepValue]
 end
 
 function Timeline_ZoomIn()
-	local currentStepValue = Chronicles.SelectedValues.timelineStep
+    local currentStepValue = Chronicles.SelectedValues.timelineStep
 
-	local curentStepIndex = GetStepValueIndex(currentStepValue)
+    local curentStepIndex = GetStepValueIndex(currentStepValue)
 
-	if (curentStepIndex == Chronicles.UI.Timeline.MaxStepIndex) then
-		return
-	end
+    if (curentStepIndex == Chronicles.UI.Timeline.MaxStepIndex) then return end
 
-	Chronicles.SelectedValues.timelineStep = Chronicles.UI.Timeline.StepValues[curentStepIndex + 1]
+    Chronicles.SelectedValues.timelineStep =
+        Chronicles.UI.Timeline.StepValues[curentStepIndex + 1]
 
-	Chronicles.SelectedValues.currentTimelinePage =
-		FindYearIndexOnTimeline(Chronicles.SelectedValues.currentTimelineYear, Chronicles.SelectedValues.timelineStep)
+    Chronicles.SelectedValues.currentTimelinePage =
+        FindYearIndexOnTimeline(Chronicles.SelectedValues.selectedTimelineYear,
+                                Chronicles.SelectedValues.timelineStep)
 
-	Chronicles.UI.Timeline:DsiplayTimeline()
+    Chronicles.UI.Timeline:DsiplayTimeline()
 end
 
 function Timeline_ZoomOut()
-	local currentStepValue = Chronicles.SelectedValues.timelineStep
+    local currentStepValue = Chronicles.SelectedValues.timelineStep
 
-	local curentStepIndex = GetStepValueIndex(currentStepValue)
+    local curentStepIndex = GetStepValueIndex(currentStepValue)
 
-	if (curentStepIndex == 1) then
-		return
-	end
+    if (curentStepIndex == 1) then return end
 
-	Chronicles.SelectedValues.timelineStep = Chronicles.UI.Timeline.StepValues[curentStepIndex - 1]
+    Chronicles.SelectedValues.timelineStep =
+        Chronicles.UI.Timeline.StepValues[curentStepIndex - 1]
 
-	Chronicles.SelectedValues.currentTimelinePage =
-		FindYearIndexOnTimeline(Chronicles.SelectedValues.currentTimelineYear, Chronicles.SelectedValues.timelineStep)
+    Chronicles.SelectedValues.currentTimelinePage =
+        FindYearIndexOnTimeline(Chronicles.SelectedValues.selectedTimelineYear,
+                                Chronicles.SelectedValues.timelineStep)
 
-	Chronicles.UI.Timeline:DsiplayTimeline()
+    Chronicles.UI.Timeline:DsiplayTimeline()
 end
 
 function FindYearIndexOnTimeline(year)
-	local length = math.abs(Chronicles.constants.timeline.yearStart - year)
-	local yearIndex = math.floor(length / Chronicles.SelectedValues.timelineStep)
+    local length = math.abs(Chronicles.constants.timeline.yearStart - year)
+    local yearIndex =
+        math.floor(length / Chronicles.SelectedValues.timelineStep)
 
-	return yearIndex - (yearIndex % Chronicles.constants.timeline.pageSize)
+    return yearIndex - (yearIndex % Chronicles.constants.timeline.pageSize)
 end
 
 ------------------------------------------------------------------------------------------
@@ -193,42 +205,27 @@ end
 ------------------------------------------------------------------------------------------
 
 function TimelineScrollFrame_OnMouseWheel(self, value)
-	--DEFAULT_CHAT_FRAME:AddMessage("-- TimelineScrollFrame_OnMouseWheel " .. value)
-	if (value > 0) then
-		TimelineScrollPreviousButton_OnClick(self)
-	else
-		TimelineScrollNextButton_OnClick(self)
-	end
+    -- DEFAULT_CHAT_FRAME:AddMessage("-- TimelineScrollFrame_OnMouseWheel " .. value)
+    if (value > 0) then
+        TimelineScrollPreviousButton_OnClick(self)
+    else
+        TimelineScrollNextButton_OnClick(self)
+    end
 end
 
 function TimelineScrollPreviousButton_OnClick(self)
-	Chronicles.SelectedValues.currentTimelinePage = Chronicles.SelectedValues.currentTimelinePage - 1
+    Chronicles.SelectedValues.currentTimelinePage =
+        Chronicles.SelectedValues.currentTimelinePage - 1
 
-	Chronicles.UI.Timeline:DsiplayTimeline()
-	PlaySound(SOUNDKIT.U_CHAT_SCROLL_BUTTON)
+    Chronicles.UI.Timeline:DsiplayTimeline()
+    PlaySound(SOUNDKIT.U_CHAT_SCROLL_BUTTON)
 end
 
 function TimelineScrollNextButton_OnClick(self)
-	Chronicles.SelectedValues.currentTimelinePage = Chronicles.SelectedValues.currentTimelinePage + 1
+    Chronicles.SelectedValues.currentTimelinePage =
+        Chronicles.SelectedValues.currentTimelinePage + 1
 
-	Chronicles.UI.Timeline:DsiplayTimeline()
-	PlaySound(SOUNDKIT.U_CHAT_SCROLL_BUTTON)
+    Chronicles.UI.Timeline:DsiplayTimeline()
+    PlaySound(SOUNDKIT.U_CHAT_SCROLL_BUTTON)
 end
 
-function SetTextToFrame(lowerBound, upperBound, frame)
-	local text = "" .. lowerBound .. "\n" .. upperBound
-	frame.lowerBound = lowerBound
-	frame.upperBound = upperBound
-
-	local label = _G[frame:GetName() .. "Text"]
-	label:SetText(text)
-
-	frame:SetScript(
-		"OnMouseDown",
-		function()
-			local eventList = Chronicles:SearchEvents(lowerBound, upperBound)
-			Chronicles.SelectedValues.currentTimelineYear = math.floor((lowerBound + upperBound) / 2)
-			Chronicles.UI.EventList:DrawEventList(frame.lowerBound, frame.upperBound, eventList)
-		end
-	)
-end
