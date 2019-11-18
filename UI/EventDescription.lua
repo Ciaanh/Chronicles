6edfc9a6af6175e75f05ed94582d4ad9f0fecf57
Nicolas Fabre <nicolas.fabre@younited-credit.fr>
2019-11-18 13:58:37 +0100

    Clean description

--- UI/EventDescription.lua
+++ UI/EventDescription.lua
@@ -3,30 +3,28 @@ local Chronicles = private.Core
 
 Chronicles.UI.EventDescription = {}
 
+
 function Chronicles.UI.EventDescription:DrawEventDescription(event)
     DEFAULT_CHAT_FRAME:AddMessage("-- Call to DrawEventDescription " .. event.label)
+
+    EventDescriptionHTML.text = event.Description[1]
 end
 
-function Chronicles.UI.EventDescription:DisplayEventDescription()
-    DEFAULT_CHAT_FRAME:AddMessage("-- Call to DisplayEventDescription ")
+
+function Chronicles.UI.EventDescription:ChangeEventDescriptionPage(page)
+    DEFAULT_CHAT_FRAME:AddMessage("-- Call to ChangeEventDescriptionPage ")
 end
 
 ------------------------------------------------------------------------------------------
 -- Description Paging --------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
 function EventDescriptionPreviousButton_OnClick(self)
-    Chronicles.SelectedValues.currentEventDescriptionPage =
-        Chronicles.SelectedValues.currentEventDescriptionPage - 1
-
-    Chronicles.UI.EventDescription:DisplayEventDescription()
+    Chronicles.UI.EventDescription:ChangeEventDescriptionPage(Chronicles.SelectedValues.currentEventDescriptionPage - 1)
     PlaySound(SOUNDKIT.U_CHAT_SCROLL_BUTTON)
 end
 
 function EventDescriptionNextButton_OnClick(self)
-    Chronicles.SelectedValues.currentEventDescriptionPage =
-        Chronicles.SelectedValues.currentEventDescriptionPage + 1
-
-    Chronicles.UI.EventDescription:DisplayEventDescription()
+    Chronicles.UI.EventDescription:ChangeEventDescriptionPage(Chronicles.SelectedValues.currentEventDescriptionPage + 1)
     PlaySound(SOUNDKIT.U_CHAT_SCROLL_BUTTON)
 end
 
