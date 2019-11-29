bf49314768714de30d3c47667484d5dcbab1a72e
Nicolas Fabre <nicolas.fabre.21@outlook.fr>
2019-11-29 23:27:39 +0100

    Merge local changes

--- DB/GlobalEventsDB.lua
+++ DB/GlobalEventsDB.lua
@@ -22,5 +22,81 @@ GlobalEventsDB = {
         yearStart = 0,
         yearEnd = 0,
         eventType = Chronicles.constants.eventType.other
+    },
+
+
+
+
+    [2] = {
+        id = 2,
+        label = Locale["Test 2"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [3] = {
+        id = 3,
+        label = Locale["Test 3"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [4] = {
+        id = 4,
+        label = Locale["Test 4"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [5] = {
+        id = 5,
+        label = Locale["Test 5"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [6] = {
+        id = 6,
+        label = Locale["Test 6"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [7] = {
+        id = 7,
+        label = Locale["Test 7"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [8] = {
+        id = 8,
+        label = Locale["Test 8"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [9] = {
+        id = 9,
+        label = Locale["Test 9"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
+    },
+    [10] = {
+        id = 10,
+        label = Locale["Test 10"],
+        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
+        yearStart = -15000,
+        yearEnd = -15000,
+        eventType = Chronicles.constants.eventType.other
     }
 }
