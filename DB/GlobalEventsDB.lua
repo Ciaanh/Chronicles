769ca57cda366a9f6bd1785294483347dab620b6
Ciaanh <nicolas.fabre.21@outlook.fr>
2020-01-12 16:17:57 +0100

    Initial basic build

--- DB/GlobalEventsDB.lua
+++ DB/GlobalEventsDB.lua
@@ -4,7 +4,7 @@ local Chronicles = private.Core
 local Locale = LibStub("AceLocale-3.0"):GetLocale(private.addon_name)
 
 GlobalEventsDB = {
-    --[[ structure:
+	--[[ structure:
 		[eventId] = {
             id=[integer],				-- Id of the event
 			label=[string], 			-- label: text that'll be the label
@@ -15,88 +15,148 @@ GlobalEventsDB = {
 
 		},
 	--]]
-    [1] = {
-        id = 1,
-        label = Locale["Dark Portal label"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = 0,
-        yearEnd = 0,
-        eventType = Chronicles.constants.eventType.other
-    },
-
-
-
-
-    [2] = {
-        id = 2,
-        label = Locale["Test 2 2 2 2 2 2  ddddddddddddddddddddddddddddddddddddddd"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [3] = {
-        id = 3,
-        label = Locale["Test 3"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [4] = {
-        id = 4,
-        label = Locale["Test 4"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [5] = {
-        id = 5,
-        label = Locale["Test 5"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [6] = {
-        id = 6,
-        label = Locale["Test 6"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [7] = {
-        id = 7,
-        label = Locale["Test 7"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [8] = {
-        id = 8,
-        label = Locale["Test 8"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [9] = {
-        id = 9,
-        label = Locale["Test 9"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    },
-    [10] = {
-        id = 10,
-        label = Locale["Test 10"],
-        description = {Locale["Dark Portal page 1"], Locale["Dark Portal page 2"]},
-        yearStart = -15000,
-        yearEnd = -15000,
-        eventType = Chronicles.constants.eventType.other
-    }
+	[1] = {
+		id = 1,
+		label = Locale["The War of the Ancients label"],
+		description = {Locale["The War of the Ancients page 1"]},
+		yearStart = -10000,
+		yearEnd = -10000,
+		eventType = Chronicles.constants.eventType.war
+	},
+	[2] = {
+		id = 2,
+		label = Locale["Quel'Thalas Founded	 label"],
+		description = {Locale["Quel'Thalas Founded page 1"]},
+		yearStart = -6800,
+		yearEnd = -6800,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[3] = {
+		id = 3,
+		label = Locale["The Troll Wars label"],
+		description = {Locale["The Troll Wars page 1"]},
+		yearStart = -2800,
+		yearEnd = -2800,
+		eventType = Chronicles.constants.eventType.war
+	},
+	[4] = {
+		id = 4,
+		label = Locale["War of the Three Hammers label"],
+		description = {Locale["War of the Three Hammers page 1"]},
+		yearStart = -230,
+		yearEnd = -230,
+		eventType = Chronicles.constants.eventType.war
+	},
+	[5] = {
+		id = 5,
+		label = Locale["The First War label"],
+		description = {Locale["The First War page 1"]},
+		yearStart = 0,
+		yearEnd = 0,
+		eventType = Chronicles.constants.eventType.war
+	},
+	[6] = {
+		id = 6,
+		label = Locale["The Fall of Stormwind label"],
+		description = {Locale["The Fall of Stormwind page 1"]},
+		yearStart = 3,
+		yearEnd = 5,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[7] = {
+		id = 7,
+		label = Locale["The Second War label"],
+		description = {Locale["The Second War page 1"]},
+		yearStart = 6,
+		yearEnd = 6,
+		eventType = Chronicles.constants.eventType.war
+	},
+	[8] = {
+		id = 8,
+		label = Locale["The Destruction of Draenor label"],
+		description = {Locale["The Destruction of Draenor page 1"]},
+		yearStart = 8,
+		yearEnd = 8,
+		eventType = Chronicles.constants.eventType.battle
+	},
+	[9] = {
+		id = 9,
+		label = Locale["The New Horde label"],
+		description = {Locale["The New Horde page 1"]},
+		yearStart = 18,
+		yearEnd = 18,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[10] = {
+		id = 10,
+		label = Locale["The Third War label"],
+		description = {Locale["The Third War page 1"]},
+		yearStart = 20,
+		yearEnd = 20,
+		eventType = Chronicles.constants.eventType.war
+	},
+	[11] = {
+		id = 11,
+		label = Locale["The Battle of Mount Hyjal"],
+		description = {Locale["The Battle of Mount Hyjal page 1"]},
+		yearStart = 21,
+		yearEnd = 21,
+		eventType = Chronicles.constants.eventType.battle
+	},
+	[12] = {
+		id = 12,
+		label = Locale["Rise of the Lich King label"],
+		description = {Locale["Rise of the Lich King page 1"]},
+		yearStart = 22,
+		yearEnd = 22,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[13] = {
+		id = 13,
+		label = Locale["The Gathering Storm label"],
+		description = {Locale["The Gathering Storm page 1"]},
+		yearStart = 25,
+		yearEnd = 25,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[14] = {
+		id = 14,
+		label = Locale["The Burning Crusade label"],
+		description = {Locale["The Burning Crusade page 1"]},
+		yearStart = 26,
+		yearEnd = 26,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[15] = {
+		id = 15,
+		label = Locale["The Wrath of the Lich King label"],
+		description = {Locale["The Wrath of the Lich King page 1"]},
+		yearStart = 27,
+		yearEnd = 27,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[16] = {
+		id = 16,
+		label = Locale["The Cataclysm label"],
+		description = {Locale["The Cataclysm page 1"]},
+		yearStart = 28,
+		yearEnd = 28,
+		eventType = Chronicles.constants.eventType.other
+	},
+	[17] = {
+		id = 17,
+		label = Locale["The Invasion of Pandaria label"],
+		description = {Locale["The Invasion of Pandaria page 1"]},
+		yearStart = 30,
+		yearEnd = 30,
+		eventType = Chronicles.constants.eventType.war
+	},
+	[18] = {
+		id = 18,
+		label = Locale["Assault of the Dark Portal label"],
+		description = {Locale["Assault of the Dark Portal page 1"]},
+		yearStart = 31,
+		yearEnd = 31,
+		eventType = Chronicles.constants.eventType.other
+	}
 }
--- Locales/frFR.lua
+++
@@ -1,8 +0,0 @@
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
-local L = AceLocale:NewLocale("Chronicles", "frFR")
-
-if L then
-    L["Chronicles"] = "Chroniques"
-    L["Description"] = "Explorez l'histoire d'Azeroth"
-    L["Icon tooltip"] = "Cliquer pour afficher la frise chronologique."
-end
