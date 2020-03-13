3354fbc21144feaafb885f639956a28248db2436
Ciaanh <nicolas.fabre.21@outlook.fr>
2020-03-13 23:58:57 +0100

    Add filter pane

---
+++ UI/EventFilter.lua
@@ -0,0 +1,12 @@
+local FOLDER_NAME, private = ...
+local Chronicles = private.Core
+
+Chronicles.UI.EventFilter = {}
+
+function Chronicles.UI.EventFilter:DisplayEventFilter(page)
+    
+end
+
+function EventFilter_OnClick(self)
+    
+end
