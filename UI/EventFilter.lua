19c66007f2899c95810b0871d4a20f3d31cf8600
Ciaanh <nicolas.fabre.21@outlook.fr>
2020-04-06 23:53:54 +0200

    Event filter display

--- UI/EventFilter.lua
+++ UI/EventFilter.lua
@@ -2,11 +2,25 @@ local FOLDER_NAME, private = ...
 local Chronicles = private.Core
 
 Chronicles.UI.EventFilter = {}
+Chronicles.UI.EventFilter.Displayed = false
 
-function Chronicles.UI.EventFilter:DisplayEventFilter(page)
-    
+function EventFilter_InitToggle(self)
+    self:SetText("<")
 end
 
-function EventFilter_OnClick(self)
+function EventFilter_Toggle()
+     -- DEFAULT_CHAT_FRAME:AddMessage("-- Toggle EventFilter " .. tostring(Chronicles.UI.EventFilter.Displayed))
+
+    if(Chronicles.UI.EventFilter.Displayed) then
+        FilterContent:Hide()
+        Chronicles.UI.EventFilter.Displayed = false
+    else
+        FilterContent:Show()
+        Chronicles.UI.EventFilter.Displayed = true
+    end
+end
+
+
+function Chronicles.UI.EventFilter:DisplayEventFilter(page)
     
 end
