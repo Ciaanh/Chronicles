c93e5df52551b2e3bdf514123ae26b8a0214d5e1
Ciaanh <nicolas.fabre.21@outlook.fr>
2025-05-25 18:55:40 +0200

    Remove unused sample db files

--- Custom/DB/02_Origins/OriginsFactionsDB.lua
+++
@@ -1,8 +0,0 @@
-local FOLDER_NAME, private = ...
-local Chronicles = private.Chronicles
-local modules = Chronicles.Custom.Modules
-local Locale = LibStub("AceLocale-3.0"):GetLocale(private.addon_name)
-
-    OriginsFactionsDB = {
-        
-    }
