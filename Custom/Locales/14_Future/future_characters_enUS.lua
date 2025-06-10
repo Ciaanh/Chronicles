4132e3a74454b3afdc63ef6d01f12630089bd79c
Ciaanh <nicolas.fabre.21@outlook.fr>
2025-06-10 22:54:52 +0200

    Remove Custom and obsolete code

--- Custom/Locales/14_Future/future_characters_enUS.lua
+++
@@ -1,6 +0,0 @@
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
-local L = AceLocale:NewLocale("Chronicles", "enUS", true, true)
-                
-        L["228_lc2"] = "lc2"
-
-        L["229_bc2"] = "bc2"
