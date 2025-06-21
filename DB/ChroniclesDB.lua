a8dfcdd0faaa66c633300b881a207a2a1aef75d5
Ciaanh <nicolas.fabre.21@outlook.fr>
2025-06-21 02:58:23 +0200

    Edit data for plugin test

--- DB/ChroniclesDB.lua
+++ DB/ChroniclesDB.lua
@@ -2,7 +2,7 @@ local FOLDER_NAME, private = ...
 local Chronicles = private.Chronicles
 Chronicles.DB = {}
 Chronicles.DB.Modules = {
-	expansions = "Expansions",
+	sample = "Sample",
 	origins = "Origins",
 	greatwars = "Greatwars",
 	worldofwarcraft = "Worldofwarcraft",
@@ -16,12 +16,13 @@ Chronicles.DB.Modules = {
 	shadowlands = "Shadowlands",
 	dragonflight = "Dragonflight",
 	future = "Future",
-	warwithin = "Warwithin"
+	warwithin = "Warwithin",
+	expansions = "Expansions"
 }
 function Chronicles.DB:Init()
-	Chronicles.Data:RegisterEventDB(Chronicles.DB.Modules.expansions, ExpansionsEventsDB)
+	Chronicles.Data:RegisterEventDB(Chronicles.DB.Modules.sample, SampleEventsDB)
 
-	Chronicles.Data:RegisterEventDB(Chronicles.DB.Modules.origins, OriginsEventsDB)
+	--Chronicles.Data:RegisterEventDB(Chronicles.DB.Modules.origins, OriginsEventsDB)
 
 	Chronicles.Data:RegisterEventDB(Chronicles.DB.Modules.future, FutureEventsDB)
 	Chronicles.Data:RegisterFactionDB(Chronicles.DB.Modules.future, FutureFactionsDB)
@@ -29,5 +30,7 @@ function Chronicles.DB:Init()
 
 	Chronicles.Data:RegisterEventDB(Chronicles.DB.Modules.warwithin, WarwithinEventsDB)
 	Chronicles.Data:RegisterCharacterDB(Chronicles.DB.Modules.warwithin, WarwithinCharactersDB)
+
+	Chronicles.Data:RegisterEventDB(Chronicles.DB.Modules.expansions, ExpansionsEventsDB)
    
 end
---
+++ DB/Locales/01_Sample/sample_events_enUS.lua
@@ -0,0 +1,10 @@
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("Chronicles", "enUS", true, true)
+                
+        L["233_sample_event_for_demo"] = "Sample event for demo"
+
+        L["234_header_1"] = "Header 1"
+
+        L["235_this_is_an_example_event__this_page_uses_only_text"] = "This is an example event, this page uses only text. On the next page you can see an example of HTML and images."
+
+        L["236_<html><body><h1>|cff0000ff_html_demo__blue_h1|r<_h"] = "<html><body><h1>|cFF0000FF HTML Demo: blue H1|r</h1><img src=\"Interface\\Icons\\Ability_Ambush\" width=\"32\" height=\"32\" align=\"right\"/><p align=\"center\">|cffee4400\'Centered text after an image from the game\'|r</p><br/><p>This is a paragraph,<br/>this is text in the same paragraph after a line break.</p><br/><br/><br/><p>This is an image from the addon, for better compatibility use power of 2 for width/height (16, 32, 64...)</p><img src=\"Interface\\AddOns\\Chronicles\\Images\\Example-image\" width=\"256\" height=\"256\" align=\"center\"/></body></html>"
